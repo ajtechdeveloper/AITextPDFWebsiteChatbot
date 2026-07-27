fa0041e2f39dfc02a9aa4b25b6f6b6f77e493e03
Aj Tech Developer <techdeveloper.aj@gmail.com>
2026-07-27 16:56:31 +0530

    Update app.py
    Removed unused import statements for HuggingFaceEmbeddings.

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from langchain_community.document_loaders import (
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain_huggingface import (
-    HuggingFaceEmbeddings,
-)
 from huggingface_hub import InferenceClient
 from langchain_core.runnables import RunnableLambda
 from langchain_community.vectorstores import FAISS
