8b62c7fbfc98b44de2ae2b3a1a03a81deaf17eae
Aj Tech Developer <techdeveloper.aj@gmail.com>
2026-07-27 16:35:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
